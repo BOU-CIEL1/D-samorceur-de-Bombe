80e2aa2993fb238540951a410cdae5dc22ad2c52
Jalil <jalilbougoffa@gmail.com>
2026-08-15 11:19:51 +0200

    v1.2
    ✨ feat:
    - Implémentation d'un système de vies (2 vies)
    - Rééquilibrage de la difficulté en changeant le nombre de fils pour les niveaux Facile (+1) et Normal (+1)
    
    ♻️ refactor:
    - Remplacement de "magic numbers" par des constantes

--- bombe.py
+++ bombe.py
@@ -6,18 +6,24 @@
 
 import random
 MINIMUM = 1
-FACILE = 2
-NORMAL = 3
+FACILE = 3
+NORMAL = 4
 DIFFICILE = 5
+GAME_OVER = 0
+
+# Création d'un dictionnaire avec les paramètres à manipuler dans le code pour pouvoir les utiliser n'importe où dans le code
+parametres = {
+  "lives": 2
+}
 
 def difficulte():
   while True:
     try:
       # L'utilisateur doit choisir son mode de difficulté entre 3 modes
       print("Choisis ton mode de difficulté: \n" \
-      "(1) Facile: 2 fils \n" \
-      "(2) Normal: 3 fils \n" \
-      "(3) Difficile: 5 fils \n")
+      f"(1) Facile: {FACILE} fils \n" \
+      f"(2) Normal: {NORMAL} fils \n" \
+      f"(3) Difficile: {DIFFICILE} fils \n")
       difficulteChoisie = int(input())
 
       # La difficulté est choisie en fonction de ce que rentre l'utilisateur et démarre le jeu
@@ -40,25 +46,36 @@ def bombe(nbFils):
   # Création d'un nombre aléatoire dans un intervalle prévu à l'avance
   nombreSecret = random.randint(MINIMUM, nbFils)
 
+  while parametres["lives"] > GAME_OVER: # Tant que l'utilisateur a encore des vies
+
   # Boucler tant que l'utilisateur n'a rien écrit
-  while True:
-    try:
-      print(f"Quel fil couper? ({MINIMUM} à {nbFils})")
-      nombreChoisi = int(input())
+    while True:
+      try:
+        print(f"Quel fil couper? ({MINIMUM} à {nbFils})")
+        nombreChoisi = int(input())
 
-      # Couvre les erreurs d'intervalles de nombre (si l'utilisateur n'a pas écris un nombre entre les valeurs demandées)
-      if MINIMUM <= nombreChoisi <= nbFils:
-        break # Saisie correcte donc on sort de la boucle while
-      else:
-        print("Erreur! Recommencez!") # Saisie incorrecte, l'utilisateur doit recommencer
+        # Couvre les erreurs d'intervalles de nombre (si l'utilisateur n'a pas écris un nombre entre les valeurs demandées)
+        if MINIMUM <= nombreChoisi <= nbFils:
+          break # Saisie correcte donc on sort de la boucle while
+        else:
+          print("Erreur! Recommencez!") # Saisie incorrecte, l'utilisateur doit recommencer
 
-    # Couvre les erreurs de types de valeur (si l'utlisateur écrit une chaine de caractères par exemple au lieu d'un entier)
-    except ValueError:
-      print("Erreur! Recommencez!")
+      # Couvre les erreurs de types de valeur (si l'utlisateur écrit une chaine de caractères par exemple au lieu d'un entier)
+      except ValueError:
+        print("Erreur! Recommencez!")
 
-  if nombreChoisi == nombreSecret:
-    print("Victoire! La bombe est désamorcée!") # Résultat en cas de victoire
+    if nombreChoisi == nombreSecret:
+      print("Victoire! La bombe est désamorcée!") # Résultat en cas de victoire
+      return
+    else:
+      lives() # Déclenchement de la fontion lives() quand l'utilisateur se trompe
+
+def lives():
+  parametres["lives"] -= 1 # L'utilisateur perd une vie s'il se trompe
+
+  if parametres["lives"] > GAME_OVER:
+    print(f"Mauvais fil, il ne te reste plus que {parametres["lives"]} vie.") # Message quand on pert une vie
   else:
-    print("BOOM! Mauvais fil...") # Résultat en cas de défaite
+    print("BOOM! Mauvais fil...") # Défaite si 0 vie
 
 difficulte()
